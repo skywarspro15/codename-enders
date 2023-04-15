7c3132c17e32d77d0ef1e8cc443741c23ebcb0ea
skwyarspro15 <potatobg648@gmail.com>
2023-04-15 22:54:09 +0800

    Initial commit

---
+++ src/main/java/net/tranchsoftware/enders/enders.java
@@ -0,0 +1,15 @@
+package net.tranchsoftware.enders;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class enders implements ModInitializer {
+	public static final String MOD_ID = "enders";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/tranchsoftware/enders/endersClient.java
@@ -0,0 +1,10 @@
+package net.tranchsoftware.enders;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class endersClient  implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
